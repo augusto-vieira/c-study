9c6120f066a747ee3496d71f5e64078e9ac7b86e
augusto-vieira <augustovieira18@gmail.com>
2025-05-03 09:35:30 -0300

    melhorando a lógica

--- CBLC/desafio_teste_final.c
+++ CBLC/desafio_teste_final.c
@@ -13,7 +13,8 @@
 
 void contar_palavras_na_linha(char *linha, size_t *total){
   char *token = strtok(linha, " \t\n");
-  while (token != NULL){
+  while (token != NULL)
+    {
       total[PALAVRAS]++;
       token = strtok(NULL, " \t\n");
     }
@@ -22,11 +23,12 @@ void contar_palavras_na_linha(char *linha, size_t *total){
 void processar_entrada(char *buffer,size_t *total, FILE *entrada){
     
     /* Realiza a leitura, linha por linha */
-    while (fgets(buffer, BUFFER_SIZE, entrada) != NULL){
+    while (fgets(buffer, BUFFER_SIZE, entrada) != NULL)
+    {
       total[BYTES] += strlen(buffer);
       contar_palavras_na_linha(buffer, total);
       total[LINHAS]++;
-      }
+    }
 }
 
 int main(int argc, char *argv[]){
@@ -35,42 +37,38 @@ int main(int argc, char *argv[]){
   size_t total[VARIAVEIS] = {0};
   FILE *entrada = stdin;
 
-  /* Verificar se a entrada é o padrão ou Redirecionamento/Pipe */
-  if(isatty(STDIN_FILENO) || argc != 1){
-    
-    if(argc == 1){
-      processar_entrada(buffer, total, entrada);
-      printf("%4zu %5zu %5zu\n", total[LINHAS], total[PALAVRAS], total[BYTES]);
-      return EXIT_SUCCESS;
-    }
-
-    for(int i = 1; i < argc; i++){
-
-      size_t total_tmp[VARIAVEIS] = {0};
-      entrada = fopen(argv[i], "r");
-      
-      if(entrada == NULL){
-        perror("Erro ao abrir o arquivo");
-        return EXIT_FAILURE;
-      }
-     
-      processar_entrada(buffer, total_tmp, entrada);
-      total[LINHAS]   += total_tmp[LINHAS];
-      total[PALAVRAS] += total_tmp[PALAVRAS];
-      total[BYTES]    += total_tmp[BYTES];
-  
-      printf("%4zu %5zu %5zu %s\n", total_tmp[LINHAS], total_tmp[PALAVRAS], total_tmp[BYTES], argv[i]);
-    }
-    if(argc > 2)
-      printf("%4zu %5zu %5zu total\n", total[LINHAS], total[PALAVRAS], total[BYTES]);
-  }
-  else{
-  
+  size_t redirecionamento = isatty(STDIN_FILENO);
+ 
+  /* Verifica se a entrada é um Redirecionamento ou Pipe */
+  if( (redirecionamento && argc == 1) || argc == 1)
+  {  
     processar_entrada(buffer, total, entrada);
-    printf("%4zu %5zu %5zu\n", total[LINHAS], total[PALAVRAS], total[BYTES]);
+    printf("%4zu %5zu %5zu\n", total[LINHAS], total[PALAVRAS], total[BYTES]); 
+  }
+  
+  else{
+      for(int i = 1; i < argc; i++)
+      {
+        size_t total_tmp[VARIAVEIS] = {0};
+        entrada = fopen(argv[i], "r");
+        
+        if(entrada == NULL)
+        {
+          perror("Erro ao abrir o arquivo");
+          return EXIT_FAILURE;
+        }
+       
+        processar_entrada(buffer, total_tmp, entrada);
+        total[LINHAS]   += total_tmp[LINHAS];
+        total[PALAVRAS] += total_tmp[PALAVRAS];
+        total[BYTES]    += total_tmp[BYTES];
     
+        printf("%4zu %5zu %5zu %s\n", total_tmp[LINHAS], total_tmp[PALAVRAS], total_tmp[BYTES], argv[i]);
+      }
+      
+      if(argc > 2)
+        printf("%4zu %5zu %5zu total\n", total[LINHAS], total[PALAVRAS], total[BYTES]);   
   }   
 
   return EXIT_SUCCESS;
 }
- 
